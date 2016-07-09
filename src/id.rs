caf8263c0b4be52c3833e99e84d1ae7ac8932a9a
Tom Parker <palfrey@tevp.net>
2016-07-09 13:04:50 +0100

    Check for a queried id that matches the searched for one

--- src/id.rs
+++ src/id.rs
@@ -2,15 +2,15 @@ use nickel::{Request, Response, MiddlewareResult};
 use hyper::client::Client;
 use common;
 use nickel::status::StatusCode;
-use json;
 use hyper::header::Location;
+use json;
 
 pub fn id_handler<'a, D>(request: &mut Request<D>,
                          mut response: Response<'a, D>)
                          -> MiddlewareResult<'a, D> {
     let client = Client::new();
-    let query = request.param("id").expect("Missing id");
-    let url = &format!("https://api.tfl.gov.uk/StopPoint/{}", query);
+    let id_query = request.param("id").expect("Missing id");
+    let url = &format!("https://api.tfl.gov.uk/StopPoint/{}", id_query);
     let obj = match common::json_for_request(client.get(url)) {
         Ok(val) => val,
         Err(val) => {
@@ -19,14 +19,13 @@ pub fn id_handler<'a, D>(request: &mut Request<D>,
         }
     };
 
-    let id = &obj["lineGroup"][0]["naptanIdReference"];
-    match id {
-        &json::JsonValue::Null => panic!("null!"),
-        &json::JsonValue::String(ref val) => {
-            response.set(Location(format!("/arrivals/{}", val)))
+    for line_group in obj["lineGroup"].members() {
+        if line_group["stationAtcoCode"].as_str().unwrap_or("") == id_query ||
+           line_group["naptanIdReference"].as_str().unwrap_or("") == id_query {
+            response.set(Location(format!("/arrivals/{}", line_group["naptanIdReference"])))
                 .set(StatusCode::PermanentRedirect);
+            return response.send("");
         }
-        _ => panic!("Something else!"),
-    };
-    response.send("")
+    }
+    response.send(json::stringify(obj))
 }
