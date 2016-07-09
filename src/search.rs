0072993cd068a30db5ef2ba01d722e1f03d2901f
Tom Parker <palfrey@tevp.net>
2016-07-09 12:05:33 +0100

    If we get a single search result, auto-redirect (fixes #6)

--- src/search.rs
+++ src/search.rs
@@ -3,6 +3,7 @@ use hyper::client::Client;
 use common;
 use mustache::MapBuilder;
 use nickel::status::StatusCode;
+use hyper::header::Location;
 
 pub fn search_handler<'a, D>(request: &mut Request<D>,
                              mut response: Response<'a, D>)
@@ -19,6 +20,12 @@ pub fn search_handler<'a, D>(request: &mut Request<D>,
         }
     };
 
+    if obj["matches"].members().count() == 1 {
+        let stop = obj["matches"].members().nth(0).unwrap();
+        response.set(Location(format!("/id/{}", stop["id"].as_str().unwrap())))
+            .set(StatusCode::PermanentRedirect);
+        return response.send("");
+    }
     let data = MapBuilder::new()
         .insert_vec("stops", |vecbuilder| {
             let mut vecb = vecbuilder;
