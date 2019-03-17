32195c9d23b8ce64f1682ef660830e20b9b92336
Tom Parker-Shemilt <palfrey@tevp.net>
2019-03-17 14:32:19 +0000

    Rewrite request/response as proper objects not lumps of JSON

--- src/main.rs
+++ src/main.rs
@@ -15,7 +15,6 @@ extern crate time;
 extern crate url;
 #[macro_use]
 extern crate lazy_static;
-#[macro_use]
 extern crate serde_json;
 
 #[cfg(test)]
@@ -33,6 +32,9 @@ use reqwest::Client;
 extern crate lambda_http;
 #[cfg(feature = "lambda")]
 extern crate aws_lambda_events;
+#[cfg(feature = "lambda")]
+#[macro_use]
+extern crate maplit;
 
 use actix_web::{http::Method, server, App, HttpRequest, HttpResponse};
 #[cfg(not(feature = "lambda"))]
@@ -128,8 +130,11 @@ mod tests {
     use super::{app, common, main};
     use actix_web::actix::System;
     use actix_web::{http, server, test, App, HttpMessage, HttpRequest, HttpResponse, State};
+    #[cfg(feature = "lambda")]
+    use aws_lambda_events::event::alb;
     use crossbeam::channel::unbounded;
     use crossbeam::{Receiver, Sender};
+    use std::collections::HashMap;
     use std::{env, thread};
 
     #[derive(Debug, Clone)]
@@ -180,30 +185,34 @@ mod tests {
     fn lambda_test() {
         let (req_send, req_recv) = unbounded();
         let (res_send, res_recv) = unbounded();
-        req_send.send(Ok((1234, json!({
-    "requestContext": {
-        "elb": {
-            "targetGroupArn": "arn:aws:elasticloadbalancing:region:123456789012:targetgroup/my-target-group/6d0ecf831eec9f09"
-        }
-    },
-    "httpMethod": "GET",
-    "path": "/",
-    "queryStringParameters": {},
-    "headers": {
-        "accept": "text/html,application/xhtml+xml",
-        "accept-language": "en-US,en;q=0.8",
-        "content-type": "text/plain",
-        "cookie": "cookies",
-        "host": "lambda-846800462-us-east-2.elb.amazonaws.com",
-        "user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6)",
-        "x-amzn-trace-id": "Root=1-5bdb40ca-556d8b0c50dc66f0511bf520",
-        "x-forwarded-for": "72.21.198.66",
-        "x-forwarded-port": "443",
-        "x-forwarded-proto": "https"
-    },
-    "isBase64Encoded": false,
-    "body": "request_body"
-})))).unwrap();
+        req_send.send(Ok((1234,
+            serde_json::to_value(
+            alb::AlbTargetGroupRequest {
+                request_context: alb::AlbTargetGroupRequestContext {
+                    elb: alb::ElbContext {
+                        target_group_arn: Some("arn:aws:elasticloadbalancing:region:123456789012:targetgroup/my-target-group/6d0ecf831eec9f09".to_string())
+                    }
+                },
+                http_method: Some("GET".to_string()),
+                path: Some("/".to_string()),
+                query_string_parameters: HashMap::new(),
+                multi_value_query_string_parameters: HashMap::new(),
+                headers: hashmap!{
+                    "accept".to_string() => "text/html,application/xhtml+xml".to_string(),
+                    "accept-language".to_string() => "en-US,en;q=0.8".to_string(),
+                    "content-type".to_string() => "text/plain".to_string(),
+                    "cookie".to_string() => "cookies".to_string(),
+                    "host".to_string() => "lambda-846800462-us-east-2.elb.amazonaws.com".to_string(),
+                    "user-agent".to_string() => "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6)".to_string(),
+                    "x-amzn-trace-id".to_string() => "Root=1-5bdb40ca-556d8b0c50dc66f0511bf520".to_string(),
+                    "x-forwarded-for".to_string() => "72.21.198.66".to_string(),
+                    "x-forwarded-port".to_string() => "443".to_string(),
+                    "x-forwarded-proto".to_string() => "https".to_string()
+                },
+                multi_value_headers: HashMap::new(),
+                is_base64_encoded: false,
+                body: Some("request_body".to_string())
+}).unwrap()))).unwrap();
         env_logger::init();
         thread::spawn(move || {
             server::new(move || test_app(req_recv.clone(), res_send.clone()).finish())
@@ -219,8 +228,7 @@ mod tests {
         env::set_var("AWS_LAMBDA_RUNTIME_API", "127.0.0.1:3456");
         thread::spawn(|| main());
         let resp_raw = res_recv.recv().unwrap();
-        let resp: aws_lambda_events::event::alb::AlbTargetGroupResponse =
-            serde_json::from_str(&resp_raw).unwrap();
+        let resp: alb::AlbTargetGroupResponse = serde_json::from_str(&resp_raw).unwrap();
         debug!("Response to main: {:#?}", resp);
         // shutdown
         req_send.send(Err(())).unwrap();
